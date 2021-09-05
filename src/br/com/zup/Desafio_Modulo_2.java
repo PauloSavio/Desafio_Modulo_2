febe11d2f93b200c13bbd59020e678fcafe365b9
Savio <pauloluizsavio@gmail.com>
2021-09-05 13:35:40 -0300

    Menu para cadastrar e sair

--- src/br/com/zup/Desafio_Modulo_2.java
+++ src/br/com/zup/Desafio_Modulo_2.java
@@ -7,26 +7,42 @@ import java.util.HashMap;
 public class Desafio_Modulo_2 {
     public static void main(String[] args) {
 
-        //Instanciando
         Map <String, String> listaDeFuncionarios = new HashMap <>(); //Eu deixei os <> vazios porque o IntelliJ deu um warning e falou que podia deixar vazio
         Scanner leitor = new Scanner(System.in);
-
-        //Pedindo informações
+        boolean chave = true;
         System.out.println("Bem vindo ao sistema de cadastro de funcionários da PauloCorp");
-        System.out.println("Digite o nome completo do funcionário:");
-        String nome = leitor.nextLine();
-        System.out.println("Digite o telefone do funcionário:");
-        String telefone = leitor.nextLine();
-        System.out.println("Digite o email do funcionário:");
-        String email = leitor.nextLine();
-        System.out.println("Digite o CPF do funcionário:");
-        String cpf = leitor.nextLine();
 
-        //Adicionando no HashMap
-        listaDeFuncionarios.put(cpf, " Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
+        int opcao = 0;
+        while (chave == true){
+            //Menu
+            System.out.println("Digite 1 para cadastrar um funcionário");
+            System.out.println("Digite 2 para sair");
+            opcao = leitor.nextInt();
+            leitor.nextLine(); //Assim o leitor lê o enter mas não armazena, aí não dá erro na mudança de int pra String
 
-        //Printando
-        System.out.println(listaDeFuncionarios);
+            if (opcao == 1){
+                //Cadastrar
+                //Pedindo informações
+                System.out.println("Digite o nome completo do funcionário:");
+                String nome = leitor.nextLine();
+                System.out.println("Digite o telefone do funcionário:");
+                String telefone = leitor.nextLine();
+                System.out.println("Digite o email do funcionário:");
+                String email = leitor.nextLine();
+                System.out.println("Digite o CPF do funcionário:");
+                String cpf = leitor.nextLine();
+
+                //Adicionando no HashMap
+                listaDeFuncionarios.put(cpf, " Nome: " + nome + " Telefone: " + telefone + " Email: " + email);
+            }
+            else if (opcao == 2){
+                //Sair
+                chave = false;
+            }
+            else{
+                System.out.println("Por favor, informe uma opção válida");
+            }
+        }
 
     }
 }
